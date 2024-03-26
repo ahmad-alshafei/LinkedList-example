92f18d2124b96d571b19b9df64c6708d6266cfa3
Salah Khaled <salah.khaled@ultimate-eg.net>
2024-03-26 10:55:26 +0200

    added marks, missing definition / fix format and typo

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,60 +1,55 @@
 #include <iostream>
 using namespace std;
 
-struct node
-{
+// MARK: - Node
+struct node {
     int data;
     node *next;
 };
+
 node *head=NULL , *last=NULL;
 int length=0;
 
+// MARK: - Definitions
 void insert(int element);
-
 void insertFirst(int element);
-
 void insertLast(int element);
-
 void remove(int val);
-
 void display();
-
 void printnode();
+void printnode2();
+void search ();
 
-void serch ();
 
+// MARK: - Main
 int main() {
     
     insert(5);
     insert(15);
     insert(25);
     insert(35);
-
+    
     display();
     remove(35);
-
     display();
-
     insert(45);
-
     display();
     
-   // printnode();
-    serch ();
-
-
+    // printnode();
+    search ();
     return 0;
 }
 
-void insert(int element){
+
+// MARK: - Functions
+void insert(int element) {
     node* newnode;
     newnode = new node;
     newnode -> data = element;
-    if (head == NULL){
+    if (head == NULL) {
         head = newnode;
         newnode -> next = NULL;
-    }
-    else{
+    } else {
         last = head;
         while (last -> next != NULL) {
             last = last -> next;
@@ -67,14 +62,13 @@ void insert(int element){
 
 
 
-void insertFirst(int element){
+void insertFirst(int element) {
     node *newnode = new node;
     newnode -> data = element;
-    if ( length == 0){
+    if ( length == 0) {
         head =last = newnode;
         newnode -> next = NULL;
-    }
-    else{
+    } else {
         last->next=newnode;
         newnode -> next = NULL;
         last = newnode;
@@ -83,22 +77,19 @@ void insertFirst(int element){
 }
 
 
-void remove(int val){
+void remove(int val) {
     node* curent, *prev;
-    if (length == 0){
-        cout << "the list is empty" << endl;
-    }
-    else if ( length == 1){
+    if (length == 0) {
+        cout << "The list is empty" << endl;
+    } else if ( length == 1) {
         curent = head;
-        if (curent ->data == val ){
+        if (curent ->data == val ) {
             head = NULL;
             free(curent);
+        } else {
+            cout << "The value not found" << endl;
         }
-        else{
-            cout << "the value not faund" << endl;
-        }
-    }
-    else{
+    } else {
         curent = prev = head;
         while (curent ->data != val) {
             prev = curent;
@@ -111,12 +102,11 @@ void remove(int val){
 }
 
 
-void display(){
+void display() {
     node* currentnode;
-    if (head == NULL){
-        cout << "the node is enpty" << endl;
-    }
-    else{
+    if (head == NULL) {
+        cout << "The node is empty" << endl;
+    } else {
         currentnode = head;
         do {
             cout << currentnode -> data << " ";
@@ -124,14 +114,14 @@ void display(){
         }
         while (currentnode -> next != NULL);
         cout << currentnode -> data << " ";
-
+        
         cout << endl;
-        cout << "The number of values ​​inside the list is = " << length << endl;
+        cout << "- Linked list count is (" << length << ")" << endl << endl;
     }
 }
 
-void printnode١(){
-    node* curr= head;
+void printnode() {
+    node* curr = head;
     int sum = 1;
     int num ;
     cin >> num;
@@ -142,14 +132,13 @@ void printnode١(){
     cout << curr ->data <<" " << endl;
 }
 
-void insertLast(int element){
+void insertLast(int element) {
     node *newnode = new node;
     newnode -> data = element;
-    if ( length == 0){
+    if ( length == 0) {
         head = last = newnode;
         newnode -> next = NULL;
-    }
-    else{
+    } else {
         head -> next = newnode;
         newnode -> next = NULL;
         head = newnode;
@@ -159,7 +148,7 @@ void insertLast(int element){
 
 
 
-void printnode(){
+void printnode2() {
     node* current = head;
     
     int sum =1;
@@ -171,10 +160,10 @@ void printnode(){
         sum++;
     }
     cout << current -> data << " " << endl;
- }
+}
 
 
-void serch (){
+void search () {
     node* current = head;
     
     int value;
@@ -182,18 +171,19 @@ void serch (){
     cin >> value;
     
     while (current ->data != value) {
-        if ( current -> next != NULL){
+        if ( current -> next != NULL) {
             current = current -> next;
-
+            
         } else {
             test = 0;
             break;
         }
     }
+    
     if (test == 1) {
         cout <<"faund it : " << current -> data << endl;
-    }
-    else{
+    } else {
         cout <<"not faund it : " << endl;
     }
 }
+
